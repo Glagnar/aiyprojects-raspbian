f44408d6e1adc7647eeb3e7f1a4fd21689231da4
Bjarke Hou <bjarke.hou@alexandra.dk>
2018-09-20 14:04:00 +0200

    demo test

--- src/alexandra/alexandra_assistant.py
+++ src/alexandra/alexandra_assistant.py
@@ -18,6 +18,9 @@ import requests
 import json
 import time
 import re
+import subprocess
+
+import platform
 
 import aiy.assistant.grpc
 import aiy.audio
@@ -31,11 +34,11 @@ logging.basicConfig(
     format="[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
 )
 
-def send_request(nick_name, project):
+def send_request(nickname, project):
     try:
-        print("Trying to reach: https://devices.alexandra.dk/webUI/sensors/nick/" + nick_name + "/" + project + ".json")
+        print("Trying to reach: https://devices.alexandra.dk/webUI/sensors/nick/" + nickname + "/" + project + ".json")
         response = requests.get(
-            url="https://devices.alexandra.dk/webUI/sensors/nick/" + nick_name + "/" + project + ".json",
+            url="https://devices.alexandra.dk/webUI/sensors/nick/" + nickname + "/" + project + ".json",
             headers={
                 "Authorization": "Basic c3BlYWtVc2VyOkxpdHRsZUJpdGNoOTM=",
             },
@@ -52,18 +55,35 @@ def send_request(nick_name, project):
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
 
-def send_message(nick_name, project, action):
-    deviceId = send_request(nick_name, project)
-    if deviceId is None:
+def send_message(nickname, project, action):
+    targetId = send_request(nickname, project)
+    if targetId is None:
         return False
 
     current_time = int(round(time.time() * 1000))
-    payload = '{"type":"command", "values":{"action":"'+action+'", "target":"'+deviceId+'"}, "sensorId":"", "time":'+str(current_time)+'}'
+    sensorId = cpu_id().stdout.decode("utf-8").strip(' \t\n\r')
+    payload = '{"type":"command", "values":{"action":"'+action+'", "target":"'+targetId+'"}, "sensorId":"'+sensorId+'", "time":'+str(current_time)+'}'
+    topic = '/iot/commands/aiy/'+targetId
     client = mqtt.Client("LOCALCONNECTION") #create new instance
     client.connect("localhost") #connect to broker
-    client.publish("/iot/commands/aiy/" + deviceId, payload)#publish$
-    print('I have send a message:\t /iot/commands/aiy/' + deviceId + ' - ' + action)
+    client.publish(topic, payload)#publish$
+    print('I have send a message:\t'+topic+' - ' + payload)
     return True
+
+def cmd_send(project, nickname, action):
+    if project != '' and nickname != '' and action != '':
+        if send_message(nickname, project, action):
+            aiy.audio.say('Message send')
+        else:
+            print('Sending message failed. No device ('+nickname+') in project '+project+'.')
+            aiy.audio.say('Sending message failed. No device with that i d.')
+    else:
+        print('You need to set a project and tell the device what to do before you can send a message. Try "give status"')
+        aiy.audio.say('Set project and command before sending a message.')
+
+def cpu_id():
+    result = subprocess.run('./cpu-id.sh', stdout=subprocess.PIPE)
+    return result
 
 def main():
     status_ui = aiy.voicehat.get_status_ui()
@@ -73,7 +93,7 @@ def main():
 
     project = ''
     action = ''
-    nick_name = ''
+    nickname = ''
     running = True
     with aiy.audio.get_recorder():
         while running:
@@ -93,40 +113,53 @@ def main():
                     project = project.replace(" ","")
                     aiy.audio.say('Project set ' + text[11:])
                     print('Project set to '+project)
+                elif text[:10] == 'set device':
+                    nickname = text[10:]
+                    nickname = nickname.replace(" ","")
+                    aiy.audio.say('Device set ' + text[10:])
+                    print('Device set to '+project)
+                elif text[-6:] == 'please':
+                    if nickname == '':
+                        print('Set device before giving simple commands. Use "set device <devicename>"')
+                        aiy.audio.say('Set device before giving simple commands.')
+                    else:
+                        action = text[:-6]
+                        aiy.audio.say('Command received: '+ action + " " + nickname + ", sending message.")
+                        cmd_send(project, nickname, action)
                 elif text[:13] == 'clear project':
                     project = ''
                     aiy.audio.say('Project cleared')
                     print('Project cleared')
                 elif text[:13] == 'clear command':
-                    nick_name = ''
+                    nickname = ''
                     action = ''
                     aiy.audio.say('Command cleared')
                     print('Command cleared')
                 elif text[:11] == 'give status':
-                    print('Project set to: \t'+project+'\nDevice set to:\t'+nick_name+'\nCommand set to:\t'+action);
-                elif text[:4] == 'tell' and project != '':
-                    # Splits the text at the key words: command, at & do.
-                    # commandData[1] = sensor nick_name
-                    # commandData[2] = action
-                    commandData = re.split('tell\s|\sto\s',text)
-                    print(commandData)
-                    action = commandData[2]
-                    nick_name = commandData[1]
-
-                    aiy.audio.say('Command received: ' + commandData[2] + " " + commandData[1])
-                elif text[:12] == 'send message':
-                    if project != '' and nick_name != '' and action != '':
-                        if send_message(commandData[1], project, commandData[2]):
-                            aiy.audio.say('Message send')
-                        else:
-                            print('Sending message failed. No device ('+nick_name+') in project '+project+'.')
-                            aiy.audio.say('Sending message failed. No device with that id.')
+                    print('Project set to: \t'+project+'\nDevice set to:\t'+nickname+'\nCommand set to:\t'+action);
+                elif text[:4] == 'tell':
+                    if project == '':
+                        print('Set project before giving commands. Use "set project <projectname>"')
+                        aiy.audio.say('Set project before giving commands.')
                     else:
-                        print('You need to set a project and tell the device what to do before you can send a message. Try "give status"')
-                        aiy.audio.say('Set project and command before sending a message.')
+                        # Splits the text at the key words: command, at & do.
+                        # commandData[1] = sensor nickname
+                        # commandData[2] = action
+                        commandData = re.split('tell\s|\sto\s',text)
+                        if len(commandData) < 3:
+                            print('Protocol is "tell <devicename> to <action>"')
+                            aiy.audio.say('Please give command in correct format.')
+                        else:
+                            print(commandData)
+                            action = commandData[2]
+                            nickname = commandData[1]
+
+                            aiy.audio.say('Command received: ' + commandData[2] + " " + commandData[1])
+                elif text[:12] == 'send message':
+                    cmd_send(project, nickname, action)
                 elif text[:7] == 'bye bye':
                     running = False
-                    aiy.audio.say('Farewell.')
+                    aiy.audio.say('Bye bye.')
                     print("Exiting program.")
                 else:
                     aiy.audio.play_audio(audio, assistant.get_volume())
